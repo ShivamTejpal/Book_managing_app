b25dcc7d7b9df9360c4de85302fe33ba76fb8cbe
Shivam Tejpal <Shivamtejpal86@gmail.com>
2024-04-11 12:16:53 +0530

    added BE

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
-const express = require('express')
+const express = require('express');
+const { addBook, updateBook } = require('./types');
 const app = express()
 
 app.use(express.json());
 
 app.post('/book_app', (req, res) => {
+    const createPayload = req.body;
+    const parsedPayload = addBook.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"You sent the wrong inputs"
+        })
+        return;
+    }
   
 })
 
@@ -12,6 +21,14 @@ app.get('/book_app', (req, res) => {
 })
 
 app.put('/completed', (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateBook.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg:"You sent the wrong inputs"
+        })
+        return;
+    }
   
 })
 
